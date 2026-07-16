62e966e1d5f702ce00f71abbc81ba32beba85a91
pankajc-07 <chutepankaj@gmail.com>
2026-07-16 17:01:50 +0530

    Update project with custom reporter and session storage tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
+import { CustomReporter } from './utils/CustomReporter';
 
 /**
  * Read environment variables from file.
@@ -23,7 +24,12 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { outputFolder: 'tta-report', open: 'never' }],
+    ['line'],
+    ['allure-playwright'],
+    ['./utils/CustomReporter.ts']  // Ensure correct path and extension
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
