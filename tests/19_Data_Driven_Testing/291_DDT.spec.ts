3620e774ea5e2a7f20a3197fa2b7c48309867760
pankajc-07 <chutepankaj@gmail.com>
2026-08-06 22:53:34 +0530

    Add data-driven testing specs, test data, and YAML/CSV utility readers

---
+++ tests/19_Data_Driven_Testing/291_DDT.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+// const userData = require('./293_Users.json');
+import userData from "./293_Users.json"; // modern JavaScript
+
+// const fs = require("fs");
+
+test('Verify Element by Filter', async ({ page }) => {
+    await page.goto("https://app.thetestingacademy.com/playwright/multiple_element_filter");
+
+    console.log(userData.username);
+    console.log(userData.password);
+
+    // const fileData = fs.readFileSync("293_Users.json", "utf-8");
+    // const user = JSON.parse(fileData);
+});
